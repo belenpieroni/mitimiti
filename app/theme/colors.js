12067dc250ad3375bb950a71cf82db8adeb2799f
belenpieroni <mbpieroni18@gmail.com>
2026-05-02 14:10:16 -0300

    agrego theme para los colores globales de la app

---
+++ app/theme/colors.js
@@ -0,0 +1,11 @@
+export const colors = {
+  background: '#EEEAE1',
+  primary: '#473472',
+  textPrimary: '#27374D',
+  textSecondary: '#526D82',
+  accent: '#9DB2BF',
+  success: '#4CAF50',
+  danger: '#E53935',
+  cardBg: '#FFFFFF',
+  tabBarBg: '#FFFFFF',
+};
